60e89982f6a63a5f98e8ab1064091b1d8d003749
Sebastian Kim <sydneyitguy@gmail.com>
2021-12-10 15:01:45 +0900

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,74 @@
+/// ENVVAR
+// - ENABLE_GAS_REPORT
+// - COMPILE_MODE
+
+require('dotenv').config();
+
+const path = require('path');
+const argv = require('yargs/yargs')()
+  .env('')
+  .boolean('enableGasReport')
+  .boolean('ci')
+  .string('compileMode')
+  .argv;
+
+require('@nomiclabs/hardhat-truffle5');
+require('@nomiclabs/hardhat-solhint');
+require('solidity-coverage');
+
+require('hardhat-gas-reporter');
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-web3');
+require("@nomiclabs/hardhat-etherscan");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: '0.8.10',
+    settings: {
+      optimizer: {
+        enabled: true, // argv.enableGasReport || argv.compileMode === 'production',
+        runs: 1500,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      blockGasLimit: 8000000,
+    },
+    bsctest: {
+      url: `https://data-seed-prebsc-1-s2.binance.org:8545/`,
+      chainId: 97,
+      gasPrice: 50000000000, // 10 GWei
+      blockGasLimit: 30000000,
+      accounts: [process.env.BSC_TEST_PRIVATE_KEY]
+    },
+    bscmain: {
+      url: `https://bsc-dataseed.binance.org`,
+      chainId: 56,
+      gasPrice: 6000000000, // 6 GWei
+      blockGasLimit: 60000000, // 6 Gwei
+      accounts: [process.env.BSC_PRIVATE_KEY]
+    }
+  },
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 20,
+    coinmarketcap: process.env.COIN_MARKET_CAP_API
+  },
+  etherscan: {
+    apiKey: process.env.BSCSCAN_API_KEY
+  }
+};
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task('accounts', 'Prints the list of accounts', async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
---
+++ scripts/deploy.js
@@ -0,0 +1,36 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  const accounts = await hre.ethers.getSigners();
+  const deployer = accounts[0].address;
+  console.log(`Deploy from account: ${deployer}`);
+
+  // MARK: - Deploy test token (only for testnet)
+  const testToken = await hre.ethers.getContractFactory('ERC20PresetMinterPauser');
+  const token = await testToken.deploy('Test Pixel', 'TEST_PIXEL');
+  await token.deployed();
+  await token.mint(deployer, '10000000000000000000000'); // 10,000 tokens
+
+  console.log(`Test token is deployed at ${token.address}`);
+
+  // MARK: - Deploy contract
+  const Pixel = await hre.ethers.getContractFactory('Pixel');
+  const pixel = await Pixel.deploy(token.address);
+  await pixel.deployed();
+
+  console.log('---');
+  console.log(`- Test token: ${token.address}`);
+  console.log(`- Pixel contract: ${pixel.address}`);
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });

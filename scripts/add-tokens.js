cd278409497c5c6021b8c3fa8ca66c2dfb5ec4d9
Sebastian Kim <sydneyitguy@gmail.com>
2022-02-09 12:44:45 +0900

    Update test code

--- scripts/add-tokens.js
+++ scripts/add-tokens.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 async function main() {
   const testToken = await hre.ethers.getContractFactory('ERC20PresetMinterPauser');
   const contract = await testToken.attach('0x62c01AF8F8Ab997Acec06C3a71DC18594726ba24');
-  await contract.mint('0x0d5B2E37FB21A784FE26fD8d89Ea3Ce8F8BDc54d', '1000000000000000000000000');
+  await contract.mint('0x91Ec1d18ed7a3587B87066F0Ab1a641dCBb84e9E', '1000000000000000000000000');
 }
 
 main()
